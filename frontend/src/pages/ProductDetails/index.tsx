d28e55a5d02d259e8f9fc90b08c8ce537520d9ba
Admilson Ferreira <ferreiraad3@gmail.com>
2023-04-24 21:11:49 -0300

    ProductDetails responsive

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -6,10 +6,10 @@ import './styles.css';
 const ProductsDetails = () => {
     return (
         <div className='product-details-container'>
-            <div className='product-details-card'>
+            <div className='base-card product-details-card'>
                 <div className='goback-container'>
                     <ArrowIcon />
-                    <h4>VOLTAR</h4>
+                    <h2>VOLTAR</h2>
                 </div>
                 <div className='row'>
                     <div className='col-xl-6'>
@@ -17,13 +17,13 @@ const ProductsDetails = () => {
                             <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
                         </div>
                         <div className='name-price-container'>
-                            <h2>Nome do produto</h2>
+                            <h1>Nome do produto</h1>
                             <ProductPrice price={2345.67} />
                         </div>
                     </div>
                     <div className='col-xl-6'>
                         <div className='description-container'>
-                            <h3>Descrição do producto</h3>
+                            <h2>Descrição do producto</h2>
                             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                         </div>
                     </div>
