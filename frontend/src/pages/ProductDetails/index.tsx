3ed326169613c189e5b80c5f3d14f8df2e96df55
Admilson Ferreira <ferreiraad3@gmail.com>
2023-04-26 00:25:56 -0300

    useState, useEffect, useParams

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,34 @@
 import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
 import ProductPrice from "components/ProductPrice";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "util/requests";
+import { Product } from "types/product";
+
 
 import './styles.css';
-import { Link } from "react-router-dom";
+import { type } from "os";
+
+type UrlParams = {
+    productId: string;
+}
 
 const ProductsDetails = () => {
+
+
+    const { productId } = useParams<UrlParams>();
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/products/${productId}`)
+            .then(response => {
+                setProduct(response.data);
+            });
+    }, [productId]);
+
+
+
     return (
         <div className='product-details-container'>
             <div className='base-card product-details-card'>
@@ -17,17 +41,17 @@ const ProductsDetails = () => {
                 <div className='row'>
                     <div className='col-xl-6'>
                         <div className='img-container'>
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+                            <img src={product?.imgUrl} alt={product?.name} />
                         </div>
                         <div className='name-price-container'>
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.67} />
+                            <h1>{product?.name}</h1>
+                            {product && <ProductPrice price={product?.price} />}
                         </div>
                     </div>
                     <div className='col-xl-6'>
                         <div className='description-container'>
                             <h2>Descrição do producto</h2>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                            <p>{product?.description} </p>
                         </div>
                     </div>
                 </div>
