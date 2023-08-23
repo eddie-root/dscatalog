897180498c9371476182bc9d608fe2a0920a151e
Admilson Ferreira <ferreiraad3@gmail.com>
2023-08-22 23:26:28 -0300

    Conditional styling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -34,7 +34,7 @@ const Login = () => {
             <h1>LOGIN</h1>
             {hasError && (
                 <div className='alert alert-danger'>
-                    Ocorreu um erro de preenchimento
+                    Ocorreu um erro, banco de dados not running...
                 </div>
             )}
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -48,7 +48,7 @@ const Login = () => {
                             }
                         })}
                         type="text"
-                        className="form-control base-input"
+                        className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
                         placeholder="Email"
                         name="username"
                     />
@@ -60,7 +60,7 @@ const Login = () => {
                             required: 'Campo obrigatório'
                         })}
                         type="password"
-                        className="form-control base-input "
+                        className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
                         placeholder="Password"
                         name="password"
                     />
