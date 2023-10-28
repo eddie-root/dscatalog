356fbf4dd9cdec64f2b5176533f3ad1e7ced1704
Admilson Ferreira <ferreiraad3@gmail.com>
2023-10-27 23:53:39 -0300

    Product crud routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <h2>
+                tela form
+            </h2>
+        </div>
+    )
+}
+
+export default Form;
