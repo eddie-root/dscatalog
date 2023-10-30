e8e90fef74c40b680b9708427a79b37cf8e8c82d
Admilson Ferreira <ferreiraad3@gmail.com>
2023-10-30 15:44:56 -0300

    Finishing productCrudCard mobile layout

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -16,16 +16,23 @@ const ProductCrudCard = ({ product }: Props) => {
                 <img src={product.imgUrl} alt={product.name} />
             </div>
             <div>
-
+                <div className="product-crud-card-bottom-container">
+                    <h6>{product.name}</h6>
+                    <ProductPrice price={product.price} />
+                </div>
+                <div className='product-crud-categories-container'>
+                    {product.categories.map((categories) => (
+                        <CategoryBadge name={categories.name} key={categories.id} />
+                    ))}
+                </div>
             </div>
-            <div className="product-crud-card-bottom-container">
-                <h6>{product.name}</h6>
-                <ProductPrice price={product.price} />
-            </div>
-            <div className='product-crud-categories-container'>
-                {product.categories.map((categories) => (
-                    <CategoryBadge name={categories.name} key={categories.id} />
-                ))}
+            <div className='product-crud-card-buttons-container'>
+                <button className='btn btn-outline-danger product-crud-card-buttom product-crud-card-buttom-first'>
+                    EXCLUIR
+                </button>
+                <button className='btn btn-outline-secondary product-crud-card-buttom'>
+                    EDITAR
+                </button>
             </div>
         </div>
     );
