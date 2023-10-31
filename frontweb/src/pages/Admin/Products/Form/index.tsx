02775e12168465a1cc90cfd1b015fc3417a880cf
Admilson Ferreira <ferreiraad3@gmail.com>
2023-10-30 21:46:19 -0300

    Form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,25 +6,33 @@ const Form = () => {
         <div className='product-crud-container'>
             <div className='base-card product-crud-form'>
                 <h1 className='product-crud-form-title'>DADOS DO PRODUTO</h1>
+
+                <form action="">
+                    <div className='row product-crud-inputs-left-container'>
+                        <div className='col-lg-6'>
+                            <div className='margin-bottom-30'>
+                                <input type="text" className='form-control base-input' />
+                            </div>
+                            <div className='margin-bottom-30'>
+                                <input type="text" className='form-control base-input' />
+                            </div>
+                            <div className='margin-bottom-30'>
+                                <input type="text" className='form-control base-input' />
+                            </div>
+
+                        </div>
+                        <div className='col-lg-6'>
+                            <div>
+                                <textarea name="" rows={10} className='form-control base-input h-auto' ></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='procut-crud-buttons-container'>
+                        <button className='btn btn-outline-danger procut-crud-button'>CANCELAR</button>
+                        <button className='btn btn-primary procut-crud-button text-white'>SALVAR</button>
+                    </div>
+                </form >
             </div>
-
-            <form action="">
-                <div className='row'>
-                    <div className='col-lg-6'>
-                        <input type="text" className='form-control base-input' />
-                        <input type="text" className='form-control base-input' />
-                        <input type="text" className='form-control base-input' />
-
-                    </div>
-                    <div className='col-lg-6'>
-                        <textarea name="" rows={10} className='form-control base-input' ></textarea>
-                    </div>
-                </div>
-                <div>
-                    <button className='btn btn-outline-danger'>CANCELAR</button>
-                    <button className='btn btn-primary'>SALVAR</button>
-                </div>
-            </form >
         </div >
     )
 }
