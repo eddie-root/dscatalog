ae54cc455b37ea2d15155bdf8ac6f390d451e665
Admilson Ferreira <ferreiraad3@gmail.com>
2023-10-31 22:16:56 -0300

    Refactor categories/imgUrl field

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -56,17 +56,11 @@ const Form = () => {
     }, [isEditing, productId, setValue]);
 
     const onSubmit = (formData: Product) => {
-        const data = {
-            ...formData,
-            imgUrl: isEditing ? formData.imgUrl
-                : '',
-            categories: isEditing ? formData.categories : [{ id: 1, name: "" }],
-        };
 
         const config: AxiosRequestConfig = {
             method: isEditing ? 'PUT' : 'POST',
             url: isEditing ? `/products/${productId}` : "/products",
-            data: data,
+            data: formData,
             withCredentials: true
         };
 
@@ -136,6 +130,23 @@ const Form = () => {
                                     name="price"
                                 />
                                 <div className='invalid-feedback d-block'>{errors.price?.message}</div>
+                            </div>
+
+                            <div className='margin-bottom-30'>
+                                <input
+                                    {...register("imgUrl", {
+                                        required: 'Campo obrigatório',
+                                        pattern: {
+                                            value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                                            message: 'Deve ser uma URL VÁLIDA'
+                                        }
+                                    })}
+                                    type="text"
+                                    className={`form-control base-input ${errors.name ? 'is-invalid' : ''}`}
+                                    placeholder="URL da imagem do produto"
+                                    name="imgUrl"
+                                />
+                                <div className='invalid-feedback d-block'>{errors.imgUrl?.message}</div>
                             </div>
 
                         </div>
